5d112e2bf9e728ea374becb982ccaf952653e829
Andy Craze <accraze@gmail.com>
2023-05-10 15:59:08 -0700

    create song title with sequential chain

--- service.py
+++ service.py
@@ -1,22 +1,31 @@
 from langchain import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain.llms import OpenAI
 
 llm = OpenAI(temperature=0.9)
 
-template = "What is a good name for a band that makes music similar to {artists} and is influenced by {influences}?"
+template = "What is a good name for a band that makes music similar to {artists}?"
 
 name_prompt = PromptTemplate(
-    input_variables=["artists", "influences"],
+    input_variables=["artists"],
     template=template,
 )
 
 name_chain = LLMChain(llm=llm, prompt=name_prompt)
 
-name = name_chain.run(
-    artists="lee scratch perry meets merzbow with the velvet underground",
-    influences="the movie metropolis",
+hit_single_prompt = PromptTemplate(
+    input_variables=["band_name"],
+    template="Write a catchy title for a hit single by the following band: {band_name}",
 )
 
+hit_single_chain = LLMChain(llm=llm, prompt=hit_single_prompt)
 
-print(name)
+# Combine the first and the second chain
+overall_chain = SimpleSequentialChain(
+    chains=[name_chain, hit_single_chain], verbose=True
+)
+
+# Run the chain specifying only the input variable for the first chain.
+hit_single = overall_chain.run(
+    "lee scratch perry meets merzbow with the velvet underground"
+)

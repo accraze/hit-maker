c257309b890f5b15059fa2f24c8847a58779eda1
Andy Craze <accraze@gmail.com>
2023-05-10 15:40:59 -0700

    add prompt template for name chain

--- service.py
+++ service.py
@@ -1,6 +1,22 @@
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 
 llm = OpenAI(temperature=0.9)
 
-text = "What would be a good band name for a group that makes music similar to lee scratch perry and merzbow?"
-print(llm(text))
+template = "What is a good name for a band that makes music similar to {artists} and is influenced by {influences}?"
+
+name_prompt = PromptTemplate(
+    input_variables=["artists", "influences"],
+    template=template,
+)
+
+name_chain = LLMChain(llm=llm, prompt=name_prompt)
+
+name = name_chain.run(
+    artists="lee scratch perry meets merzbow with the velvet underground",
+    influences="the movie metropolis",
+)
+
+
+print(name)
